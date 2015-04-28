5b6707f89417360e5ec51aeff3fc99be0a9c12a7
Waldo Boshoff (Mac) <wakkerakker@gmail.com>
2015-04-28 09:55:31 +0200

    first commit getting the app up there

---
+++ www/assets/js/route.js
@@ -0,0 +1,569 @@
+route = {
+    "driver_name": "Pieter Moeilik",
+    "vehicle_registration": "NUR 1124",
+    "status": "success",
+    "trip": {
+        "stops": [
+            {
+                "ATD": null,
+                "StopName": "DURBAN",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T06:24:25",
+                "longitude": 30.984664,
+                "ETA": null,
+                "StopId": 12,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [],
+                "latitude": -29.780989,
+                "StopComments": "No Comments",
+                "StopTAT": 0,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": "Completed",
+                "StopSequence": 0
+            },
+            {
+                "ATD": "2015-04-12T07:05:00",
+                "StopName": "246 Crescent Bakery",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:05:00",
+                "longitude": 30.89369269,
+                "ETA": "2015-04-12T07:00:00",
+                "StopId": 15,
+                "ATA": "2015-04-12T07:05:00",
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 282
+                    }
+                ],
+                "latitude": -29.90582533,
+                "StopComments": "No Comments",
+                "StopTAT": 5,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Completed"
+                ],
+                "StopSequence": 1
+            },
+            {
+                "ATD": null,
+                "StopName": "17 Sunset Supermarket",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:13:00",
+                "longitude": 30.89463918,
+                "ETA": "2015-04-12T07:05:00",
+                "StopId": 17,
+                "ATA": "2015-04-12T07:13:00",
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 210
+                    }
+                ],
+                "latitude": -29.9056943,
+                "StopComments": "No Comments",
+                "StopTAT": 8,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Arrived"
+                ],
+                "StopSequence": 2
+            },
+            {
+                "ATD": null,
+                "StopName": "66 Daniels Tuck Shop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:19:02",
+                "longitude": 30.89885309,
+                "ETA": "2015-04-12T07:14:02",
+                "StopId": 19,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 259
+                    }
+                ],
+                "latitude": -29.90592279,
+                "StopComments": "No Comments",
+                "StopTAT": 5,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 3
+            },
+            {
+                "ATD": null,
+                "StopName": "20 Bangladash Tea Room",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:27:12",
+                "longitude": 30.89655032,
+                "ETA": "2015-04-12T07:22:12",
+                "StopId": 21,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 211
+                    }
+                ],
+                "latitude": -29.91147425,
+                "StopComments": "No Comments",
+                "StopTAT": 5,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 4
+            },
+            {
+                "ATD": "2015-04-12T07:34:10",
+                "StopName": "62 Checkout",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:34:10",
+                "longitude": 30.89781819,
+                "ETA": "2015-04-12T07:28:10",
+                "StopId": 23,
+                "ATA": "2015-04-12T07:34:10",
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 255
+                    }
+                ],
+                "latitude": -29.91211835,
+                "StopComments": "No Comments",
+                "StopTAT": 6,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Completed"
+                ],
+                "StopSequence": 5
+            },
+            {
+                "ATD": null,
+                "StopName": "16 Take and Pay",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:46:10",
+                "longitude": 30.89776642,
+                "ETA": "2015-04-12T07:34:10",
+                "StopId": 25,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 209
+                    }
+                ],
+                "latitude": -29.91209367,
+                "StopComments": "No Comments",
+                "StopTAT": 12,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 6
+            },
+            {
+                "ATD": null,
+                "StopName": "1 Spartak Convinience",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T07:58:47",
+                "longitude": 30.90268208,
+                "ETA": "2015-04-12T07:48:47",
+                "StopId": 27,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 197
+                    }
+                ],
+                "latitude": -29.91356862,
+                "StopComments": "No Comments",
+                "StopTAT": 10,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 7
+            },
+            {
+                "ATD": null,
+                "StopName": "22 A Pillay",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T08:04:34",
+                "longitude": 30.90348267,
+                "ETA": "2015-04-12T08:00:34",
+                "StopId": 29,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 198
+                    }
+                ],
+                "latitude": -29.91086172,
+                "StopComments": "No Comments",
+                "StopTAT": 4,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 8
+            },
+            {
+                "ATD": null,
+                "StopName": "14 Ryson Tuck Shop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T08:11:23",
+                "longitude": 30.89805169,
+                "ETA": "2015-04-12T08:08:23",
+                "StopId": 31,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 207
+                    }
+                ],
+                "latitude": -29.90693586,
+                "StopComments": "No Comments",
+                "StopTAT": 3,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 9
+            },
+            {
+                "ATD": null,
+                "StopName": "265 Cavendish Supermarket",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T08:21:00",
+                "longitude": 30.90904655,
+                "ETA": "2015-04-12T08:15:00",
+                "StopId": 33,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 256
+                    }
+                ],
+                "latitude": -29.89982696,
+                "StopComments": "No Comments",
+                "StopTAT": 6,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 10
+            },
+            {
+                "ATD": null,
+                "StopName": "335 Bread Ahead",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T09:34:00",
+                "longitude": 31.00596236,
+                "ETA": "2015-04-12T09:27:00",
+                "StopId": 34,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 243
+                    }
+                ],
+                "latitude": -29.70134222,
+                "StopComments": "No Comments",
+                "StopTAT": 7,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 11
+            },
+            {
+                "ATD": null,
+                "StopName": "13 Goolams Service Station Caltex",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T10:14:05",
+                "longitude": 31.00373768,
+                "ETA": "2015-04-12T10:06:05",
+                "StopId": 35,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 206
+                    }
+                ],
+                "latitude": -29.70046757,
+                "StopComments": "No Comments",
+                "StopTAT": 8,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 12
+            },
+            {
+                "ATD": null,
+                "StopName": "95 Luckys Convinience Store",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T10:26:50",
+                "longitude": 31.01618914,
+                "ETA": "2015-04-12T10:19:50",
+                "StopId": 36,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 280
+                    }
+                ],
+                "latitude": -29.69120523,
+                "StopComments": "No Comments",
+                "StopTAT": 7,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 13
+            },
+            {
+                "ATD": null,
+                "StopName": "3 Bp Woodview",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T10:44:52",
+                "longitude": 31.02726377,
+                "ETA": "2015-04-12T10:32:52",
+                "StopId": 37,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 241
+                    }
+                ],
+                "latitude": -29.68799229,
+                "StopComments": "No Comments",
+                "StopTAT": 12,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 14
+            },
+            {
+                "ATD": null,
+                "StopName": "100 One Stop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T10:57:02",
+                "longitude": 31.01994954,
+                "ETA": "2015-04-12T10:50:02",
+                "StopId": 38,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 287
+                    }
+                ],
+                "latitude": -29.68335488,
+                "StopComments": "No Comments",
+                "StopTAT": 7,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 15
+            },
+            {
+                "ATD": null,
+                "StopName": "99 Yolandas Tuck",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T11:10:20",
+                "longitude": 31.0118878,
+                "ETA": "2015-04-12T11:01:20",
+                "StopId": 39,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 284
+                    }
+                ],
+                "latitude": -29.67805755,
+                "StopComments": "No Comments",
+                "StopTAT": 9,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 16
+            },
+            {
+                "ATD": null,
+                "StopName": "58 Shell Brookdale Service Station",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T11:29:43",
+                "longitude": 31.00332708,
+                "ETA": "2015-04-12T11:17:43",
+                "StopId": 40,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 251
+                    }
+                ],
+                "latitude": -29.68447992,
+                "StopComments": "No Comments",
+                "StopTAT": 12,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 17
+            },
+            {
+                "ATD": null,
+                "StopName": "454 Carltons Tuck Shop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T11:35:42",
+                "longitude": 31.00114612,
+                "ETA": "2015-04-12T11:31:42",
+                "StopId": 41,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 252
+                    }
+                ],
+                "latitude": -29.68316521,
+                "StopComments": "No Comments",
+                "StopTAT": 4,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 18
+            },
+            {
+                "ATD": null,
+                "StopName": "103 Maligas Tuck Shop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T11:49:12",
+                "longitude": 30.99389723,
+                "ETA": "2015-04-12T11:40:12",
+                "StopId": 42,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 293
+                    }
+                ],
+                "latitude": -29.69000883,
+                "StopComments": "No Comments",
+                "StopTAT": 9,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 19
+            },
+            {
+                "ATD": null,
+                "StopName": "48 Seras Tuck Shop",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T12:07:42",
+                "longitude": 30.98195484,
+                "ETA": "2015-04-12T11:59:42",
+                "StopId": 43,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [
+                    {
+                        "orderStatus": "Awaiting",
+                        "orderId": 234
+                    }
+                ],
+                "latitude": -29.71443129,
+                "StopComments": "No Comments",
+                "StopTAT": 8,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 20
+            },
+            {
+                "ATD": null,
+                "StopName": "DURBAN",
+                "StopClose": "13:30:00",
+                "ETD": "2015-04-12T12:55:42",
+                "longitude": 30.984664,
+                "ETA": "2015-04-12T12:37:42",
+                "StopId": 44,
+                "ATA": null,
+                "StopOpen": "07:00:00",
+                "orders": [],
+                "latitude": -29.780989,
+                "StopComments": "No Comments",
+                "StopTAT": 18,
+                "StopType": "Delivery",
+                "StopAddress": "Address Here",
+                "Status": [
+                    "Awaiting"
+                ],
+                "StopSequence": 21
+            }
+        ]
+    }
+}
